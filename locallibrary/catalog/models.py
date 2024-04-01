78181a8d4fb0874a05b453d8f3b76d69d8644e50
cale-ndsu <149906571+cale-ndsu@users.noreply.github.com>
2024-04-01 07:35:34 -0500

    Fixed a few things from module eight and finished module nine (forms).

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 from django.urls import reverse  # To generate URLS by reversing URL patterns
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 
 
